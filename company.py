8722f88fa9e4b54053552e2d4fd1f5c023c74d97
mks3011 <bmb3011@gmail.com>
2024-03-09 18:14:52 +0300

    done

--- company.py
+++ company.py
@@ -45,7 +45,7 @@ departments = [
         "employers": [
             {"first_name": "Christina", "last_name": "Walker", "position": "Python dev", "salary_rub": 80000},
             {"first_name": "Michelle", "last_name": "Gilbert", "position": "JS dev", "salary_rub": 85000},
-            {"first_name": "Caitlin", "last_name": "Bradley", "position": "Teamlead", "salary_rub": 950000},
+            {"first_name": "Caitlin", "last_name": "Bradley", "position": "Teamlead", "salary_rub": 95000},
             {"first_name": "Brian", "last_name": "Hartman", "position": "CTO", "salary_rub": 130000},
         ]
     },
@@ -56,3 +56,82 @@ taxes = [
     {"department": "IT Department", "name": "hiring", "value_percents": 6},
     {"department": "BizDev Department", "name": "sales", "value_percents": 20},
 ]
+
+
+def employers_info(key, key2=None):  # функция выводит список со значениями переданного ключа в списке employers
+    value_list = []
+    for value in departments:
+        department_title = value["title"]
+        for v in value["employers"]:
+            if key2 is None:
+                name = v[key]
+                value_list.append(name)
+            if key2 == "title":
+                name_department = v[key] + " (" + department_title + ")"
+                value_list.append(name_department)
+            if key2 == "salary_rub":
+                name = v["first_name"]
+                salary = v["salary_rub"]
+                value_list.append({"name": name, "salary": salary})
+    return value_list
+print(employers_info("first_name", "salary_rub"))
+
+# 1. Вывести названия всех отделов
+print("Названия отделов:", ', '.join(department["title"] for department in departments))
+
+# 2. Вывести имена всех сотрудников компании.
+print("Имена всех сотрудников компании:", ', '.join(employers_info("first_name")))
+
+# 3. Вывести имена всех сотрудников компании с указанием отдела, в котором они работают.
+print("Имена всех сотрудников компании с указанием отдела:", ', '.join(employers_info("first_name", "title")))
+
+# 4. Вывести имена всех сотрудников компании, которые получают больше 100к.
+names = [item["name"] for item in (employers_info("first_name", "salary_rub")) if item["salary"] > 100000]
+print(names)
+
+
+
+
+
+
+# # 1. Вывести названия всех отделов
+# for department in departments:
+#     print("Названия отделов:", department["title"])
+# # print(", ".join(department["title"] for department in departments))
+#
+# # 2. Вывести имена всех сотрудников компании.
+# for department in departments:
+#     for personnel in department["employers"]:
+#         print(personnel["first_name"], personnel["last_name"])
+#
+# # 3. Вывести имена всех сотрудников компании с указанием отдела, в котором они работают.
+# for department in departments:
+#     for personnel in department["employers"]:
+#         print(f'{personnel["first_name"]} {personnel["last_name"]} работает в {department["title"]}')
+#
+# # 4. Вывести имена всех сотрудников компании, которые получают больше 100к.
+# for department in departments:
+#     for personnel in department["employers"]:
+#         if personnel["salary_rub"] > 100000:
+#             print(personnel["first_name"], personnel["last_name"])
+#
+# # 5. Вывести позиции, на которых люди получают меньше 80к (можно с повторениями).
+# for department in departments:
+#     for position in department["employers"]:
+#         if position["salary_rub"] < 100000:
+#             print(position["position"])
+#
+# # 6. Посчитать, сколько денег в месяц уходит на каждый отдел – и вывести вместе с названием отдела
+# for department in departments:
+#     total_salary = 0
+#     for salary in department["employers"]:
+#         total_salary += salary["salary_rub"]
+#     print(f'{department["title"]} {total_salary}')
+
+# 7. Вывести названия отделов с указанием минимальной зарплаты в нём.
+
+# 8. Вывести названия отделов с указанием минимальной, средней и максимальной зарплаты в нём.
+# 9. Вывести среднюю зарплату по всей компании.
+# 10. Вывести названия должностей, которые получают больше 90к без повторений.
+# 11. Посчитать среднюю зарплату по каждому отделу среди девушек (их зовут Мишель, Николь, Кристина и Кейтлин).
+# 12. Вывести без повторений имена людей, чьи фамилии заканчиваются на гласную букву.
